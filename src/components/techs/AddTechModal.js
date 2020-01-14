76093a48285bac28b2af959923addf392fd0bbd8
Robert Zettl <zettl.robert@gmail.com>
2020-01-14 15:43:08 +0100

    Edit and Tech Modal Components TechItem to List all Techs

---
+++ src/components/techs/AddTechModal.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+function AddTechModal() {
+  //State
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [tech, setTech] = useState('');
+
+
+
+  const onSubmit = () => {
+    //Error Checking Modal Input
+    if (firstName === '' || lastName === '') {
+      M.toast({ html: 'Please enter first and last name' });
+    } else {
+      console.log(firstName, lastName);
+
+
+      // Clear all Fields
+      setFirstName('');
+      setLastName('');
+    }
+  }
+
+  return (
+    // div needs id from link to open as modal + material className modal
+    <div id="add-tech-modal" className="modal">
+      <div className="modal-content">
+        <h4>New Technician</h4>
+        <div className="row">
+          <div className="input-field">
+            <input
+              type="text"
+              name="firstName"
+              value={firstName}
+              onChange={e => setFirstName(e.target.value)}
+            />
+            <label htmlFor="firstName" className="active">
+              FirstName
+            </label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field">
+            <input
+              type="text"
+              name="lastName"
+              value={lastName}
+              onChange={e => setLastName(e.target.value)}
+            />
+            <label htmlFor="lastName" className="active">
+              lastName
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="modal-footer">
+        <a href="#!"
+          onClick={onSubmit}
+          className="modal-close waves-effect waves-teal btn"
+          style={modalFooterStyle}>
+          Enter
+        </a>
+      </div>
+    </div>
+  )
+}
+
+const modalFooterStyle = {
+  margin: '0 24px'
+}
+
+export default AddTechModal
---
+++ src/components/techs/TechListModal.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+import TechItem from './TechItem'
+
+const TechListModal = () => {
+  // State
+  const [techs, setTechs] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getTechs();
+    //es-lint-disable-next-line
+  }, [])
+
+  const getTechs = async () => {
+    setLoading(true);
+
+    // Request to Backend with Fetch API -- added proxy no localhost/5000 required
+    const res = await fetch('/techs');
+    const data = await res.json();
+
+    setTechs(data);
+    setLoading(false);
+  }
+
+  return (
+    <div id="tech-list-modal" className="modal">
+      <div className="modal-content">
+        <h4>Technician List</h4>
+        <ul className="collection">
+          {!loading && techs.map(tech => (
+            <li className="collection-item">
+              <TechItem tech={tech} key={tech.id} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default TechListModal
