785d83ab94bab800fb43131890e1b08bd70f0466
Robert Zettl <zettl.robert@gmail.com>
2020-01-15 13:47:36 +0100

    Implemented searchLogs

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -27,28 +27,34 @@ export default (state = initialState, action) => {
         ...state,
         logs: action.payload,
         loading: false
-      }
+      };
 
     case ADD_LOG:
       return {
         ...state,
         logs: [...state.logs, action.payload],
         loading: false
-      }
+      };
 
     case DELETE_LOG:
       return {
         ...state,
         logs: state.logs.filter(log => log.id !== action.payload),
         loading: false
-      }
+      };
 
     case UPDATE_LOG:
       return {
         ...state,
         logs: state.logs.map(log =>
           log.id === action.payload.id ? action.payload : log)
-      }
+      };
+
+    case SEARCH_LOGS:
+      return {
+        ...state,
+        logs: action.payload
+      };
 
     case SET_CURRENT:
       return {
@@ -66,14 +72,14 @@ export default (state = initialState, action) => {
       return {
         ...state,
         loading: true
-      }
+      };
 
     case LOGS_ERROR:
       console.error(action.payload);
       return {
         ...state,
         error: action.payload
-      }
+      };
 
     default:
       return state;
